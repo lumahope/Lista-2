4b98066e8e0679f21d334cd0f87dc5d40e90c8b0
lumahope <55193164+lumahope@users.noreply.github.com>
2025-09-02 12:42:20 -0400

    Update Q12.c

--- Q12.c
+++ Q12.c
@@ -1,10 +1,15 @@
-/*Faça um programa que receba três notas de um aluno, calcule e imprima a média*/
+/* Faça um programa que receba três notas de um aluno, calcule e imprima a média
+aritmética entre essas três notas e uma mensagem que segue a tabela abaixo:
+Média Mensagem
+0.0 |__ 5.0 reprovado
+5.0 |__ 7.0 exame
+7.0 |__| 10.0 aprovado */
 #include <stdio.h>
 
 int main() {
     float nota1, nota2, nota3, media;
 
-    // Recebe as três notas
+    // Recebe as três notas do aluno
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
@@ -14,11 +19,21 @@ int main() {
     printf("Digite a terceira nota: ");
     scanf("%f", &nota3);
 
-    // Calcula a média
+    // Calcula a média aritmética
     media = (nota1 + nota2 + nota3) / 3;
 
-    // Imprime o resultado
-    printf("\nA media das notas e: %.2f\n", media);
+    // Imprime a média
+    printf("\nA media do aluno e: %.2f\n", media);
+
+    // Verifica a situação do aluno com base na média e imprime a mensagem
+    if (media >= 7.0 && media <= 10.0) {
+        printf("Mensagem: Aprovado\n");
+    } else if (media >= 5.0 && media < 7.0) {
+        printf("Mensagem: Exame\n");
+    } else {
+        printf("Mensagem: Reprovado\n");
+    }
 
     return 0;
 }
+
